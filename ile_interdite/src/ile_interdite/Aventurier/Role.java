61d211e09bfc9a6b90651abd6bcd2f7a38308761
Valentin TOSI <valentin.tosi@gmail.com>
2017-12-12 11:25:37 +0100

    Création de l'enum 'Role'.

---
+++ ile_interdite/src/ile_interdite/Aventurier/Role.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ile_interdite.Aventurier;
+
+/**
+ *
+ * @author tosiv
+ */
+public enum Role
+{
+    AVENTURIER("Aventurier"),
+    EXPLORATEUR("Explorateur"),
+    INGENIEUR("Ingenieur"),
+    MESSAGER("Messager"),
+    NAVIGATEUR("Navigateur"),
+    PILOTE("Pilote"),
+    PLONGEUR("Plongeur");
+    
+    private final String nomRole;
+
+    Role(String nomRole)
+    {
+        this.nomRole = nomRole;
+    }
+
+    @Override
+    public String toString()
+    {
+        return this.nomRole ;
+    }
+
+    static Role getFromName(String name)
+    {
+        if (AVENTURIER.name().equals(name)) return AVENTURIER ;
+        if (EXPLORATEUR.name().equals(name)) return EXPLORATEUR ;
+        if (INGENIEUR.name().equals(name)) return INGENIEUR ;
+        if (MESSAGER.name().equals(name)) return MESSAGER ;
+        if (NAVIGATEUR.name().equals(name)) return NAVIGATEUR ;
+        if (PILOTE.name().equals(name)) return PILOTE ;
+	if (PLONGEUR.name().equals(name)) return PLONGEUR ;
+        return null ;
+    }
+}
